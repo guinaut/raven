7de4269c0da4836f2236439d72eca84c92f9c0a0
Matthew Wyman <matt@okareo.com>
2024-11-23 11:21:43 -0800

    Added PostHog Identify

--- src/app/aerie/layout.tsx
+++ src/app/aerie/layout.tsx
@@ -1,9 +1,25 @@
 'use client';
 
+import { useEffect } from 'react';
 import { AppShell } from '@mantine/core';
 import { AerieFooter } from '@/components/commons';
+import { useAuth, useUser } from '@clerk/nextjs';
+import { usePostHog } from 'posthog-js/react';
 
 export default function AppLayout({ children }: React.PropsWithChildren) {
+	const posthog = usePostHog();
+	const { isSignedIn, userId } = useAuth();
+	const { user } = useUser();
+
+	useEffect(() => {
+		if (isSignedIn && userId && user && !posthog._isIdentified()) {
+			posthog.identify(userId, {
+				email: user.primaryEmailAddress?.emailAddress,
+				username: user.username,
+			});
+		}
+	}, [posthog, user, isSignedIn, userId]);
+
 	return (
 		<AppShell withBorder={true}>
 			<AppShell.Main mb={50}>
